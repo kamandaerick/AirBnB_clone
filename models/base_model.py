5b4f91221390a4efe8f4b5d972032f134b8828c2
Kamanda <info.proresearchers@gmail.com>
2023-10-14 23:07:43 -0700

    Solution to task 5

--- models/base_model.py
+++ models/base_model.py
@@ -4,47 +4,56 @@ import uuid
 from datetime import datetime
 
 
-class BaseModel(object):
+class BaseModel:
+    __fmt_datetime = "%Y-%m-%dT%H:%M:%S.%f"
 
-    def __init__(self, *args, **kwargs):
-        """
-        Initialize a new instance of the BAseModel class.
-        Generate a unique ID, set creation and updation time
-        """
-        if kwargs:
-            for key, value in kwargs.items():
-                if key != "__class__":
-                    setattr(self, key, value)
-            self.created_at = datetime.fromisoformat(kwargs["created_at"])
-            self.updated_at = datetime.fromisoformat(kwargs["updated_at"])
-        else:
+    def __init__(self, *args, **kwargs) -> None:
+        """Initializes the instance of this class 'BaseModel'"""
+        from models import storage  # noqa
+        if not kwargs:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
+            storage.new(self)
+        else:
+            for k, v in kwargs.items():
+                if k != '__class__':
+                    if k == 'updated_at' or k == 'created_at':
+                        v = datetime.strptime(v, self.__fmt_datetime)
+                    # self.__dict__.update({k: v})
+                    setattr(self, k, v)
 
-    def __str__(self):
+    @classmethod
+    def get_name(cls) -> str:
+        """Returns the name of the class"""
+        return cls.__name__
+
+    def __str__(self) -> str:
         """
         Return a human readable string representation of the object
         including class name, ID, and a dictionary of object attributes
         """
-        return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
+        return f"[{self.get_name()}] ({self.id}) {self.__dict__}"
 
-    def save(self):
+    def save(self) -> None:
         """
         Update the "updated_at" attribute with current datetime
         """
-        self.updated_at = datetime.now()
+        from models import storage  # noqa
 
-    def to_dict(self):
+        self.updated_at = datetime.now()
+        storage.save()
+
+    def to_dict(self) -> dict:
         """
         Convert an object into a dictionary
         Include all instance attributes, class name, and time created
         and updated in isoformat
         Returns a dictionary containing all keyvalues of __dict__ of instance
         """
-        data = self.__dict__.copy()
-        data["__class__"] = self.__class__.__name__
-        data["created_at"] = data["created_at"].isoformat()
-        data["updated_at"] = data["updated_at"].isoformat()
-
-        return data
+        d = {}
+        d['__class__'] = self.get_name()
+        d.update(self.__dict__)
+        d['created_at'] = self.created_at.strftime(self.__fmt_datetime)
+        d['updated_at'] = self.updated_at.strftime(self.__fmt_datetime)
+        return d
---
+++ models/engine/file_storage.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""This module defines the storage class"""
+import json
+import os
+
+from models.base_model import BaseModel
+
+
+class FileStorage:
+    __file_path = 'file.json'
+    __objects = {}
+
+    def all(self) -> dict:
+        """Returns the dictionary __Ojects"""
+        return self.__objects
+
+    def new(self, obj: BaseModel):
+        """
+        Sets a new objects
+        In __objects the obj with key <obj class name>.id
+        """
+        k = f"{obj.get_name()}.{obj.id}"
+        self.__objects[k] = obj.to_dict()
+
+    def save(self):
+        """Serialzies __objects to a json file"""
+        json_object = json.dumps(self.__objects)
+
+        with open(self.__file_path, 'w') as fp:
+            fp.write(json_object)
+
+    def reload(self):
+        """Deserializes a json file to __objects"""
+        if os.path.exists(self.__file_path):
+            with open(self.__file_path) as user_file:
+                self.__objects = json.load(user_file)
