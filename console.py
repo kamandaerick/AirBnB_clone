0fb3429ef6f2e6374d5991c44f6754728138dcaa
Erick Kamanda <info.proresearchers@gmail.com>
2024-05-16 20:27:56 +0300

    Imlemented the show, destroy, all and update commands in my interpreter

--- console.py
+++ console.py
@@ -42,10 +42,69 @@ class HBNBCommand(cmd.Cmd):
         elif len(tokens) == 1:
             print("** instance id missing **")
             return
+        else:
+            instance_attribute = f"{tokens[0]}.{tokens[1]}"
+            if not instance_attribute in FileStorage.objects_copy:
+                print("** no instance found **")
+            else:
+                instance = FileStorage.objects_copy[instance_attribute]
+                print(instance)
+
+    def do_destroy(self, line):
+        """Delete an instance based on the class name and id"""
+        tokens = line.split()
+        if not tokens:
+            print("** class name missing **")
+            return
+        elif tokens[0] not in globals():
+            print("** class doesn't exist **")
+            return
+        elif len(tokens) == 1:
+            print("** instance id missing **")
+            return
         instance_attribute = f"{tokens[0]}.{tokens[1]}"
         if instance_attribute not in FileStorage.objects_copy:
             print("** no instance found **")
-        instance = FileStorage.objects_copy[instance_attribute]
-        print(instance)
+        else:
+            del FileStorage.objects_copy[instance_attribute]
+            FileStorage.save(FileStorage)
+
+    def do_all(self, line):
+        """Print string representation of all instances based or not on the class name"""
+        tokens = line.split()
+        if not tokens:
+            print([str(obj) for obj in FileStorage.objects_copy.values()])
+        elif tokens[0] not in globals():
+            print("** class doesn't exist **")
+            return
+        else:
+            print([str(obj) for obj in FileStorage.objects_copy.values() if type(obj) == globals()[tokens[0]]])
+    
+    def do_update(self, line):
+        """Update an instance based on the class name and id by adding or updating attribute"""
+        tokens = line.split()
+        if not tokens:
+            print("** class name missing **")
+            return
+        elif tokens[0] not in globals():
+            print("** class doesn't exist **")
+            return
+        elif len(tokens) == 1:
+            print("** instance id missing **")
+            return
+        instance_attribute = f"{tokens[0]}.{tokens[1]}"
+        if instance_attribute not in FileStorage.objects_copy:
+            print("** no instance found **")
+            return
+        elif len(tokens) == 2:
+            print("** attribute name missing **")
+            return
+        elif len(tokens) == 3:
+            print("** value missing **")
+            return
+        setattr(FileStorage.objects_copy[instance_attribute], tokens[2], tokens[3])
+        FileStorage.save(FileStorage)
+    
+    
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
