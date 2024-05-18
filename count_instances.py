508c8ca353c62b387ad2afb236bb3fd4a2ceaa08
Erick Kamanda <info.proresearchers@gmail.com>
2024-05-18 10:56:54 +0300

    Created a function to dynamically extract the status of objects to get the correct count and monitor changes

--- count_instances.py
+++ count_instances.py
@@ -4,50 +4,56 @@
 from models.engine.file_storage import FileStorage
 import re
 
-text = ""
-for k, v in FileStorage.objects_copy.items():
-    text += str(v)
+# text = ""
+# for k, v in FileStorage.objects_copy.items():
+#     text += str(v)
+def get_text():
+    """Generate the text representation of all objects"""
+    text = ""
+    for k, v in FileStorage.objects_copy.items():
+        text += str(v)
+    return text
 
 def count_city():
     """Count the number of instances of City"""
     pattern = re.compile(r"\bCity\b")
-    matches = pattern.findall(text)
+    matches = pattern.findall(get_text())
     return len(matches)
 
 def count_user():
     """Count the number of instances of User"""
     pattern = re.compile(r"\bUser\b")
-    matches = pattern.findall(text)
+    matches = pattern.findall(get_text())
     return len(matches)
 
 def count_review():
     """Count the number of instances of Review"""
     pattern = re.compile(r"\bReview\b")
-    matches = pattern.findall(text)
+    matches = pattern.findall(get_text())
     return len(matches)
 
 def count_place():
     """Count the number of instances of Place"""
     pattern = re.compile(r"\bPlace\b")
-    matches = pattern.findall(text)
+    matches = pattern.findall(get_text())
     return len(matches)
 
 def count_amenity():
     """Count the number of instances of Amenity"""
     pattern = re.compile(r"\bAmenity\b")
-    matches = pattern.findall(text)
+    matches = pattern.findall(get_text())
     return len(matches)
 
 def count_state():
     """Count the number of instances of State"""
     pattern = re.compile(r"\bState\b")
-    matches = pattern.findall(text)
+    matches = pattern.findall(get_text())
     return len(matches)
 
 def count_base_model():
     """Count the number of instances of BaseModel"""
     pattern = re.compile(r"\bBaseModel\b")
-    matches = pattern.findall(text)
+    matches = pattern.findall(get_text())
     return len(matches)
 
 def all_instances():
